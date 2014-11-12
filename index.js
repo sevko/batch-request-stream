bfb6cfb36e9890dbf63546a624725dd1e34c0e97
Severyn Kozak <severyn.kozak@gmail.com>
2014-11-12 10:41:54 -0500

    Add functional implementation of the batch-stream.
    index.js
    	-Add a functional implementation of the batch-request stream.
    	-Undocumented/lacking unit-tests, though.

---
+++ index.js
@@ -0,0 +1,51 @@
+"use strict";
+
+var stream = require("stream");
+
+function createStream(request, batchSize, maxLiveRequests){
+	var batchSize = batchSize || 100;
+	var batch = [];
+
+	var maxLiveRequests = maxLiveRequests || 100;
+	var liveRequests = 0;
+
+	var streamEnded = false;
+	var streamPaused = false;
+
+	var writeStream = new stream.Writable();
+
+	function requestCompleted(){
+		writeStream.emit("requestCompleted");
+	}
+
+	writeStream.on("requestCompleted", function requestCompleted(){
+		liveRequests--;
+		if(streamPaused && liveRequests < maxLiveRequests){
+			streamPaused = false;
+			writeStream.emit("resumeStream");
+		}
+	});
+
+	writeStream._write = function _write(data, enc, next){
+		batch.push(data);
+		if(batch.length == batchSize){
+			liveRequests++;
+			request(batch, requestCompleted);
+			batch = [];
+
+			if(liveRequests >= maxLiveRequests){
+				streamPaused = true;
+				this.once("resumeStream", next);
+			}
+			else {
+				next();
+			}
+		}
+		else {
+			next();
+		}
+	};
+	return writeStream;
+}
+
+module.exports = createStream;
