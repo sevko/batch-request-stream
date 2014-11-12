05c14c12e99e2e464be261cc42d86e56d81e574e
Severyn Kozak <severyn.kozak@gmail.com>
2014-11-12 11:47:24 -0500

    Add rate-limiting unit test.
    test/test.js
    	-Add a TAP test for rate-limiting.

--- test/test.js
+++ test/test.js
@@ -9,6 +9,7 @@ var tap = require("tape");
 
 tap("Test batch-sizes.", function callback(test){
 	var batchSize = 4;
+	var numItems = batchSize * 5;
 
 	function request(batch, requestCompleted){
 		test.equal(batch.length, batchSize, "Batch size matches.");
@@ -16,13 +17,42 @@ tap("Test batch-sizes.", function callback(test){
 	}
 
 	var batchRequestStream = new createBatchRequestStream(
-		request, batchSize, 2,
-		{
-			objectMode: true
-		}
+		request, batchSize, 2, {objectMode: true}
 	);
-	for(var num = 0; num < batchSize * 10; num++){
+	for(var num = 0; num < numItems; num++){
 		batchRequestStream.write(num);
 	}
 	test.end();
+});
+
+tap("Test rate-limiting.", function callback(test){
+	var batchSize = 4;
+	var numItems = batchSize * 5;
+	var numLiveRequests = 0;
+	var maxLiveRequests = 2;
+	var sumRequests = 0;
+
+	function request(batch, requestCompleted){
+		var currRequestNum = ++sumRequests;
+		numLiveRequests++;
+		test.ok(
+			numLiveRequests <= maxLiveRequests,
+			"Number of live requests less than maxLiveRequests."
+		);
+		setTimeout(function handleRequest(){
+			numLiveRequests--;
+			requestCompleted();
+			if(currRequestNum == numItems / batchSize){
+				test.end();
+			}
+		}, 100);
+	}
+
+	var batchRequestStream = new createBatchRequestStream(
+		request, batchSize, maxLiveRequests, {objectMode: true}
+	);
+
+	for(var num = 0; num < numItems; num++){
+		batchRequestStream.write(num);
+	}
 });
